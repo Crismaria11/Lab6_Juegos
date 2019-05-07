da3b3f380ad8ec3f7224d5c96c65bf4a089cca61
Cristina B <bau161260@uvg.edu.gt>
2019-05-06 19:50:32 -0600

    Ya solo falta que cuando se toque el enemigo se destruya el jugador

---
+++ Lab6/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(enemy == null)
+        {
+            enemy = this.gameObject.GetComponent<NavMeshAgent>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(target.position); 
+    }
+}
---
+++ Lab6/Assets/Scripts/Recorrido.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recorrido : MonoBehaviour
+{
+    public Transform recorrido;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Recorrer")
+        {
+            other.gameObject.GetComponent<EnemyController>().target = recorrido;
+        }
+    }
+}
